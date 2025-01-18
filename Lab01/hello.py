cd78c08ed7cf984f31f227eb8b9d4a87b8089a2e
IconiqCS <sungacharles28@gmail.com>
2025-01-18 20:08:46 +0800

    F1-Done

--- Lab01/hello.py
+++
@@ -1,2 +0,0 @@
-name = "Charles!"
-print("Hello, " + name)
---
+++ [F1-TECHNICAL]/F1-Technical.py
@@ -0,0 +1,13 @@
+
+print("================")
+firstName = input("Enter first name: ")
+middleName = input("Enter middle name: ")
+lastName = input("Enter last name: ")
+birthday = input("Enter birthday: ")
+gender = input("Enter gender: ")
+address = input("Enter address: ")
+course = input("Enter course: ")
+yearLevel = input("Enter year level: ")
+print("================")
+print("First Name: " + firstName, "\nMiddle Name: " + middleName, "\nLast Name: " + lastName, 
+      "\nBirthday: " + birthday, "\nGender: " + gender, "\nAddress: " +  address, "\nCourse: " + course, "\nYear Level: " + yearLevel)
