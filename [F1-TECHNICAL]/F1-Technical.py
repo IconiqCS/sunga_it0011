1f1b29f8ea9b5d1e60599c09ace68837ca0e5939
IconiqCS <sungacharles28@gmail.com>
2025-01-18 20:10:13 +0800

    18/01/2025 - 8:11pm

--- [F1-TECHNICAL]/F1-Technical.py
+++ [F1-TECHNICAL]/F1-Technical.py
@@ -1,5 +1,5 @@
 
-print("================")
+print("=================")
 firstName = input("Enter first name: ")
 middleName = input("Enter middle name: ")
 lastName = input("Enter last name: ")
@@ -8,6 +8,6 @@ gender = input("Enter gender: ")
 address = input("Enter address: ")
 course = input("Enter course: ")
 yearLevel = input("Enter year level: ")
-print("================")
+print("=================")
 print("First Name: " + firstName, "\nMiddle Name: " + middleName, "\nLast Name: " + lastName, 
       "\nBirthday: " + birthday, "\nGender: " + gender, "\nAddress: " +  address, "\nCourse: " + course, "\nYear Level: " + yearLevel)
